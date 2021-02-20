5194d6541b422297ec4e88c16c9185135fa2d8ad
Remy Ghalayini <remyghalayini@gmail.com>
2021-02-20 12:22:06 +0100

    added search and filter function

--- app.js
+++ app.js
@@ -2,74 +2,106 @@
 
     $('#searchButton').click(function(){
         let searchValue = $('#searchQuery').val();
-        const search_url = `https://swapi.dev/api/people/?search=${searchValue}`
-        //console.log(search_url);
-        let data = getapi(search_url);
-        //let count = data.value.count;
-        console.log(data);
-        // for(var i = 0; i < data.length; i++) {
-        //     var obj = data[i];
-        
-        //     console.log(i);
-        // }
-        //for (let i=0; i< data.count; i++){
-            
-        //    console.log(i);
-        // }
-
-        //$('.search-list').append($('<li>').text().on("click", SelectCharacter()));
-
-
+        const search_url = `https://swapi.dev/api/people/?search=${searchValue}`;
+        fetch(search_url)
+        .then((resp) => resp.json())
+        .then(function(data) {
+            console.log("data");
+            console.log(data);
+            for(var i = 0; i < data.results.length; i++) {
+                $('.search-list').append($('<li>')
+                                    .text(data.results[i].name)
+                                .append($('<a>')
+                                    .addClass('click-character')
+                                    .text('Select')
+                                    .val(data.results[i].url)
+                                    .on('click', SelectCharacter)
+                                    ));
+                console.log(data.results[i].name);
+            }
+        })
+        .catch(function(error) {
+            console.log("error");
+            console.log(error);
+        });
     });
 
-    // function SelectCharacter(){
-    //     //const api_url ='https://swapi.dev/api/people/1'   
-    //       // Defining async function 
-    //     async function getapi(url) { 
+    function SelectCharacter(){
+         const url = $(this).val();
+         fetch(url)
+            .then((resp) => resp.json())
+            .then(function(data) {
+                $("#name").text(data.name);
+                $("#birth-year").text(data.birth_year);
+                var homeworld_site = data.homeworld;
+
+                SelectHomeworld(homeworld_site);
+            })
+        .catch(function(error) {
+            console.log("error");
+            console.log(error);
+        });           
+    };
+
+    function SelectHomeworld(url){
+        fetch(url)
+            .then((resp) => resp.json())
+            .then(function(homeworld) {
+                $("#homeworld").text(homeworld.name);
+        })
+        .catch(function(error) {
+            console.log("error");
+            console.log(error);
+        });           
+    }
+    //function SelectCharacter(){
+        // const api_url ='https://swapi.dev/api/people/1'   
+        //   // Defining async function 
+        // async function getapi(url) { 
                     
+        // // Storing response 
+        // const response = await fetch(url); 
+                    
+        //     // Storing data in form of JSON 
+        //     var data = await response.json(); 
+        //     console.log(data); 
+        //     if (response) { 
+        //         hideloader(); 
+        //         } 
+        //         //let us take the name of the homeworld planet
+        //         let homeworld_site = data.homeworld;
+    
+        //         const response_homeworld = await fetch(homeworld_site);                    
+        //         var homeworld = await response_homeworld.json(); 
+    
+        //         //show(data); 
+        //         $("#name").text(data.name);
+        //         $("#birth-year").text(data.birth_year);
+        //         $("#homeworld").text(homeworld.name);
+        //         } 
+        
+        
+        //         // Function to hide the loader 
+        //     function hideloader() { 
+        //         document.getElementById('loading').style.display = 'none'; 
+        //     } 
+        
+        //     // Calling that async function 
+        //     getapi(api_url); 
+    //}
+
+    // async function getapi(url) {             
     //     // Storing response 
-    //     const response = await fetch(url); 
-                    
+    //     const response = await fetch(url);                    
     //         // Storing data in form of JSON 
     //         var data = await response.json(); 
-    //         console.log(data); 
+    //         //console.log(data); 
     //         if (response) { 
     //             hideloader(); 
-    //             } 
-    //             //let us take the name of the homeworld planet
-    //             let homeworld_site = data.homeworld;
-    
-    //             const response_homeworld = await fetch(homeworld_site);                    
-    //             var homeworld = await response_homeworld.json(); 
-    
-    //             //show(data); 
-    //             $("#name").text(data.results);
-    //             $("#birth-year").text(data.birth_year);
-    //             $("#homeworld").text(homeworld.name);
-    //             } 
+    //         }
+    //     return data; 
         
-        
-    //             // Function to hide the loader 
-    //         function hideloader() { 
-    //             document.getElementById('loading').style.display = 'none'; 
-    //         } 
-        
-    //         // Calling that async function 
-    //         getapi(api_url); 
-    // }
-
-    async function getapi(url) {             
-        // Storing response 
-        const response = await fetch(url);                    
-            // Storing data in form of JSON 
-            var data = await response.json(); 
-            //console.log(data); 
-            if (response) { 
-                hideloader(); 
-            }
-        return data; 
-        
-    } 
+    // } 
     
     // Function to hide the loader 
     function hideloader() { 
